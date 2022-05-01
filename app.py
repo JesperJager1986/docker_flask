8d8f0b2cd97f2fc721c7112e1e8a270ef0c8d572
JesperJager1986 <jesperjag86@gmail.com>
2022-05-01 14:59:38 +0200

    the basics is working

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    return 'test'
+    return '<h1>Jeg elsker Anne-Cathrine</h1>'
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    homepage()
+    pass
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
